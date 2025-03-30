8006cb8755568b875da0855127eb9429856b0bf4
sirpy <hadar@gooddollar.org>
2025-03-31 01:11:32 +0300

    fix: max logs

--- packages/bridge-app/src/sdk.ts
+++ packages/bridge-app/src/sdk.ts
@@ -315,10 +315,7 @@ export class BridgeSDK {
     /// get the latest added block as checkpoint
     //TODO: need to verify latest checkpoint has enough signers according to the bridge contract requirements
     const latestCheckpointFilter = this.registryContract.filters.BlockAdded(null, sourceChainId);
-    const events = await this.registryContract.queryFilter(
-      latestCheckpointFilter,
-      -(this.registryBlockFrequency * 100),
-    );
+    const events = await this.registryContract.queryFilter(latestCheckpointFilter, -(this.registryBlockFrequency * 5));
     const bestBlock = maxBy(events, (_) => _.args?.blockNumber);
 
     if (bestBlock) return bestBlock.args?.blockNumber.toNumber() as number;
